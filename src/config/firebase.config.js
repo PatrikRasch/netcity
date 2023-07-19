34dad85813c328f02f1affd8f3369874c1c5ddef
PatrikRasch <patrikrask@hotmail.com>
2023-07-19 22:09:22 +0200

    Basic auth and db set up

---
+++ src/config/firebase.config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDAdQFrVmnFOfX8B4W_J5sIBMHAhBgGvrU",
+  authDomain: "top-netcity.firebaseapp.com",
+  projectId: "top-netcity",
+  storageBucket: "top-netcity.appspot.com",
+  messagingSenderId: "240729259839",
+  appId: "1:240729259839:web:ab67d5608c3ef35a005d8a",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
+export const db = getFirestore(app);
