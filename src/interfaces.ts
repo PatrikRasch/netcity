3c32c6bfe9c365e413dbcdcc4290c9a65c401062
PatrikRasch <patrikrask@hotmail.com>
2023-07-24 14:34:57 +0200

    Added TargetData for types expected from Firebase within the Post component. Used in the getPostData function.

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,27 +1,41 @@
+import firebase from "firebase/compat/app";
+
 export interface userIdProp {
   userId: string;
   setUserId: (value: string) => void;
 }
 
 export interface PostData {
-  id?: string;
+  id: string;
   firstName: string;
   lastName: string;
   text: string;
   date: string;
-  likes: number;
-  dislikes: number;
+  likes: object;
+  dislikes: object;
   comments: number;
 }
 
-export interface postProp {
+export interface PostProp {
   firstName: string;
   lastName: string;
   postText: string;
   postDate: string;
-  postNumOfLikes: number;
-  postNumOfDislikes: number;
+  postLikes: object;
+  postDislikes: object;
   postNumOfComments: number;
+  userId: string;
+  postId: string;
+}
+
+export interface TargetData {
+  comments: number;
+  date: string;
+  firstName: string;
+  lastName: string;
+  likes: object;
+  text: string;
+  timestamp: firebase.firestore.Timestamp;
 }
 
 export interface FirstNameProp {
