0e5ee17006d316099cfb85366854281b7734fc84
PatrikRasch <patrikrask@hotmail.com>
2023-07-25 17:00:08 +0200

    Profile button in header now text instead of an image

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import profilePicture from "./../assets/images/profile-picture.jpg";
+// import emptyProfilePicture from "./../assets/icons/emptyProfilePicture.jpg";
 
 const Header = () => {
+  // const [profilePicture, setProfilePicture] = useState(emptyProfilePicture);
   const navigate = useNavigate();
   return (
     <div>
       <div className="h-[100px]  grid grid-cols-3 items-center text-center text-3xl">
-        <div className="">Logo</div>
+        <div className="" onClick={() => navigate("/people")}>
+          People
+        </div>
         <div className="cursor-pointer justify-self-center" onClick={() => navigate("/public")}>
           Public
         </div>
-        <img
+        <div onClick={() => navigate("/profile")}>Profile</div>
+        {/* <img
           src={profilePicture}
           alt=""
           className="rounded-[50px] justify-self-center cursor-pointer aspect-square object-cover max-h-[65px]"
           onClick={() => navigate("/profile")}
-        />
+        /> */}
       </div>
       <div className="h-1 bg-black w-[100vw]"></div>
     </div>
