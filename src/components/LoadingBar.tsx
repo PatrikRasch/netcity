ac8cf7ec991ad53e47d432f8ecec8568bfc38441
PatrikRasch <patrikrask@hotmail.com>
2023-09-28 18:28:37 +0200

    style: update loading bar to a more suitable one for the page design

--- src/components/LoadingBar.tsx
+++ src/components/LoadingBar.tsx
@@ -1,21 +1,34 @@
-import React from "react";
-import "./loadingBar.css";
+import React from 'react'
+import './loadingBar.css'
+
+import logoIcon from '../assets/icons/logoIcon.png'
 
 function LoadingBar() {
   return (
-    <div>
-      <div className="lds-roller">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+    <div className="relative h-[150px] w-[150px]">
+      <div className="absolute left-1/2 top-1/2 h-[120px] w-[120px] translate-x-[-48.8%] translate-y-[-50%] rounded-[50%] bg-white"></div>
+      <img src={logoIcon} alt="" className="absolute left-1/2 top-1/2 w-[70px] translate-x-[-50%] translate-y-[-50%]" />
+      <div className="lds-ring h-full w-full">
         <div></div>
         <div></div>
         <div></div>
         <div></div>
       </div>
     </div>
-  );
+  )
 }
 
-export default LoadingBar;
+export default LoadingBar
+
+{
+  /* <div className="lds-roller">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div> */
+}
