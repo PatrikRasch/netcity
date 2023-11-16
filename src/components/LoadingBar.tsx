f7f3da8abc8bb70e4240927321b6f8c14de728b6
PatrikRasch <patrikrask@hotmail.com>
2023-11-16 20:48:00 +0100

    style: add props for custom size and colour of spinner

--- src/components/LoadingBar.tsx
+++ src/components/LoadingBar.tsx
@@ -1,28 +1,25 @@
 import React from 'react'
 import './loadingBar.css'
 
-import logoIcon from '../assets/icons/logoIcon.png'
-
 interface Props {
   scale?: number
   height?: number
   width?: number
-  color?: string
+  circle?: string
 }
 
-function LoadingBar({ scale, height, width, color }: Props) {
-  if (scale === undefined) scale = 100
-  if (height === undefined) height = 150
-  if (width === undefined) width = 150
-  if (color === undefined) color = 'default'
+function LoadingBar({ scale, height, width, circle }: Props) {
   return (
-    <div className={`relative flex h-[${height}px] w-[${width}px] scale-[${scale}]`}>
-      {/* <div className="absolute left-1/2 top-1/2 h-[120px] w-[120px] translate-x-[-48.8%] translate-y-[-50%] rounded-[50%] bg-white"></div> */}
-      {/* <img src={logoIcon} alt="" className="absolute left-1/2 top-1/2 w-[70px] translate-x-[-50%] translate-y-[-50%]" /> */}
+    <div
+      className={`relative flex ${scale ? `scale-[${scale}]` : 'scale-100'} ${
+        height ? `h-[${height}px]` : 'h-[150px]'
+      } ${width ? `w-[${width}px]` : 'w-[150px]'}`}
+    >
       <div
-        className={`lds-ring flex h-full w-full items-center justify-center ${
-          color !== 'default' ? 'lds-ring-black' : 'lds-ring-default-color'
-        }`}
+        className={`lds-ring default flex h-full w-full items-center justify-center
+        ${circle ? '' : 'default'}
+        ${circle === 'smallAndBlack' ? 'small-and-black' : ''}
+        `}
       >
         <div></div>
         <div></div>
@@ -46,4 +43,11 @@ export default LoadingBar
         <div></div>
         <div></div>
       </div> */
+}
+
+{
+  /* <div className="absolute left-1/2 top-1/2 h-[120px] w-[120px] translate-x-[-48.8%] translate-y-[-50%] rounded-[50%] bg-white"></div> */
+}
+{
+  /* <img src={logoIcon} alt="" className="absolute left-1/2 top-1/2 w-[70px] translate-x-[-50%] translate-y-[-50%]" /> */
 }
